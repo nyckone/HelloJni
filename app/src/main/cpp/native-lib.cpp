1fe84d12db4176b94f7060079c48f71d1c5469d9
Doron Hogery <doron.hogery@gmail.com>
2017-05-31 02:15:38 +0300

    Created a jni function that gets a number and returns the corresponding propertie according to the format requested

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -11,15 +11,25 @@ Java_com_example_doron_hellojni_MainActivity_stringFromJNI(
     int n = 0;
     char *name = (char *) malloc(PROP_NAME_MAX * sizeof(char));
     char *value = (char *) malloc(PROP_VALUE_MAX * sizeof(char));
+    std::string name_value = "";
+    std::string name_str = "";
+    std::string value_str = "";
 
     const prop_info *prop = __system_property_find_nth(0);
     n = __system_property_read(prop, name, value);
 
     if (0 == n) {
+        free(name);
+        free(value);
         return env->NewStringUTF(hello.c_str());
     }
 
+    name_str = name;
+    value_str = value;
+    name_value = name_str + ":" + value_str;
+
+    free(name);
     free(value);
 
-    return env->NewStringUTF(name);
+    return env->NewStringUTF(name_value.c_str());
 }
